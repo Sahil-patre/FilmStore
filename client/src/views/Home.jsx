68f4f6b24ed6df09bdad2df8ea3145319c0ccc51
Sahil-patre <sahilpatre54@gmail.com>
2025-11-06 11:14:15 +0530

    search integration

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+import { Search as SearchIcon } from "lucide-react";
+import toast, { Toaster } from 'react-hot-toast';
 import MovieCard from "./../components/MovieCard.jsx";
 
 function Home() {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState("");
 
   const loadMovies = async () => {
     const response = await axios.get("http://localhost:8080/movies");
@@ -14,8 +18,48 @@ function Home() {
     loadMovies();
   }, []);
 
+  const searchMovies = async () => {
+  toast.loading("Searching...", { id: "searching" });
+
+  try {
+    const response = await axios.get(
+      `http://localhost:8080/movies/search?q=${search}`
+    );
+    toast.dismiss();
+    setMovies(response.data.data);
+    setError("");
+  } catch (error) {
+    console.log(error);
+    toast.dismiss();
+    toast.error(error.response.data.message, { id: "error" });
+    setMovies([]);
+    setError(error.response.data.message);
+  }
+};
+
+  useEffect(() => {
+    searchMovies();
+  }, [search]);
+
   return (
     <div>
+
+    <div className="flex justify-center mt-6">
+  <div className="flex items-center border border-gray-400 rounded-lg px-3 py-2 w-80 bg-gray-100 focus-within:ring-2 focus-within:ring-blue-400">
+    <input
+      type="text"
+      placeholder="Search for a movie..."
+      className="bg-transparent flex-grow text-gray-800 placeholder-gray-500 focus:outline-none"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+    />
+    <SearchIcon className="text-blue-500 cursor-pointer" />
+  </div>
+  
+</div>
+
+{error ? <div className="text-red-500 text-center mt-4">{error}</div> : null}
+
       <div className="flex flex-wrap gap-6 justify-around p-6">
       {movies.map((movieObj) => {
         const { _id, title, images, category, year, rating } = movieObj;
@@ -31,6 +75,7 @@ function Home() {
         );
       })}
     </div>
+    <Toaster position="top-right"/>
     </div>
   );
 }
